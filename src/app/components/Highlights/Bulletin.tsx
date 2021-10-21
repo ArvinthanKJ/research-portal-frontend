d77755d4f5b33c2c1c1252286ecefa4ffaeb77ac
Shankar JP <shankarjp12@outlook.com>
2021-10-21 13:45:50 +0530

    Added Bulletin Section

--- src/app/components/Highlights/Bulletin.tsx
+++ src/app/components/Highlights/Bulletin.tsx
@@ -3,6 +3,7 @@ import { axiosInstance } from "../../utils/axios";
 import { useHistory } from "react-router";
 import { Labs } from "../../interfaces/projects";
 import NoResults from "../NoResults/NoResults";
+import './highlights.css';
 
 const Bulletin = () => {
   const history = useHistory();
@@ -14,34 +15,23 @@ const Bulletin = () => {
   };
 
   return (
-    <div className="wrapper mt-10 mb-10 p-2">
-      <p className="text-center text-red-500 xl:text-5xl lg:text-4xl md:text-3xl text-2xl pb-3 font-semibold">
-        INNOVATIONS
-      </p>
-      <div className="border-solid border-4 border-red-200 m-1">
-        <div className="bg-white p-6 rounded-lg shadow-lg m-0.5">
-          <h2 className="xl:text-3xl lg:text-2xl md:text-xl text-lg text-gray-800 font-semibold">
-            Insert cool Heading
-          </h2>
-          <p className="xl:text-xl lg:text-lg md:text-sm text-xs text-gray-700 ">
-            This is my cool description!
-          </p>
+    <div>
+      <p className="text-center text-red-500 xl:text-5xl lg:text-4xl md:text-3xl text-2xl pb-3 font-bold publication-head">Publications</p>
+      <div className="border-solid border-4 border-red-200 m-1 publication-box">
+        <div className="bg-white p-6 rounded-lg shadow-lg m-0.5 pub-div">
+          <h2 className="xl:text-3xl lg:text-2xl md:text-xl text-lg text-gray-800 font-semibold pub-div-head">Insert Publication Name</h2>
+          <h3 className="xl:text-xl lg:text-lg md:text-sm text-xs text-gray-700 pub-div-author">Author</h3>
+          <p className="pub-div-ref"><a href="/">Reference Link</a></p>
         </div>
-        <div className="bg-white p-6 rounded-lg shadow-lg m-0.5">
-          <h2 className="xl:text-3xl lg:text-2xl md:text-xl text-lg text-gray-800 ">
-            Insert cool Heading
-          </h2>
-          <p className="xl:text-xl lg:text-lg md:text-sm text-xs text-gray-700">
-            This is my cool description!
-          </p>
+        <div className="bg-white p-6 rounded-lg shadow-lg m-0.5 pub-div">
+          <h2 className="xl:text-3xl lg:text-2xl md:text-xl text-lg text-gray-800 font-semibold pub-div-head">Insert Publication Name</h2>
+          <h3 className="xl:text-xl lg:text-lg md:text-sm text-xs text-gray-700 pub-div-author">Author</h3>
+          <p className="pub-div-ref"><a href="/">Reference Link</a></p>
         </div>
-        <div className="bg-white p-6 rounded-lg shadow-lg m-0.5">
-          <h2 className="xl:text-3xl lg:text-2xl md:text-xl text-lg text-gray-800 f">
-            Insert cool Heading
-          </h2>
-          <p className="xl:text-xl lg:text-lg md:text-sm text-xs text-gray-700">
-            This is my cool description!
-          </p>
+        <div className="bg-white p-6 rounded-lg shadow-lg m-0.5 pub-div">
+          <h2 className="xl:text-3xl lg:text-2xl md:text-xl text-lg text-gray-800 font-semibold pub-div-head">Insert Publication Name</h2>
+          <h3 className="xl:text-xl lg:text-lg md:text-sm text-xs text-gray-700 pub-div-author">Author</h3>
+          <p className="pub-div-ref"><a href="/">Reference Link</a></p>
         </div>
       </div>
     </div>
--- src/app/components/Highlights/index.tsx
+++ src/app/components/Highlights/index.tsx
@@ -1,10 +1,12 @@
 import Innovations from "./Innovations";
 import Departments from "../Home/Departments";
+import Bulletin from "./Bulletin";
 const Highlights = () => {
   return (
     <div className="w-ful bg-red-50 relative">
       <Innovations />
       <Departments />
+      <Bulletin />
     </div>
   );
 };
